066c7ff7688e72dd6afa4c380c5719446d590b3b
Cliburn Chan <cliburn.chan@duke.edu>
2015-03-19 20:13:18 -0400

    Rearrange to avoid py.test errors

--- Labs/Lab07/Reproducible/tests/distance.py
+++ Labs/Lab07/Reproducible/tests/distance.py
@@ -2,6 +2,10 @@
 import numpy as np
 
 def euclidean_dist(u, v):
-    """Returns Euclidean distance betwen numpy vectors u and v."""
     w = u - v
-    return np.sqrt(np.sum(w**2))
+    return np.sum(np.sqrt(w**2))
+    
+# def euclidean_dist(u, v):
+#     """Returns Euclidean distance betwen numpy vectors u and v."""
+#     w = u - v
+#     return np.sqrt(np.sum(w**2))
